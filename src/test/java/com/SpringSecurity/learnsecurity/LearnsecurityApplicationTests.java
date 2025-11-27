1a76d72d490cdc72dcdd25424dade8cb544d8be6
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2025-11-27 11:40:07 +0530

    Initialize project with Spring Security starter and basic auth/authz explanation

---
+++ src/test/java/com/SpringSecurity/learnsecurity/LearnsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurity.learnsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
